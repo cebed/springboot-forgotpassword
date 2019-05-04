8f3349815e22b59b23fa4ee35416f8a2e62f94d5
cebed <36077080+cebed@users.noreply.github.com>
2019-05-04 13:49:59 +0200

    personrepository

---
+++ src/main/java/com/example/demopassword/repository/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.example.demopassword.repository;
+
+
+import com.example.demopassword.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository("personRepository")
+public interface PersonRepository extends JpaRepository<Person, Long> {
+        Optional<Person> findByEmail(String email);
+        Optional<Person> findByResetToken(String resetToken);
+        }
